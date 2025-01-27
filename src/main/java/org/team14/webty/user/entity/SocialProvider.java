5939c64918731cc5fca454f21fa461e27d8a3e5a
daydreaam <dayingyeo@naver.com>
2025-01-27 17:19:49 +0900

    init SocialProvider & WebtyUser

--- src/main/java/org/team14/webty/user/entity/SocialProvider.java
+++ src/main/java/org/team14/webty/user/entity/SocialProvider.java
@@ -1,4 +1,36 @@
 package org.team14.webty.user.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class SocialProvider {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long socialId;
+
+	@NotNull
+	private String provider;
+
+	@NotNull
+	private String providerId;
+
+	@OneToOne
+	@JoinColumn(name = "webty_user_id")
+	private WebtyUser webtyUser;
 }
--- src/main/java/org/team14/webty/user/entity/WebtyUser.java
+++ src/main/java/org/team14/webty/user/entity/WebtyUser.java
@@ -1,4 +1,36 @@
 package org.team14.webty.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class WebtyUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long webtyUserId;
+
+	@NotNull
+	@Column(unique = true)
+	private String nickname;
+
+	@NotNull
+	private String profileImg;
+
+	@OneToOne(mappedBy = "webtyUser", fetch = FetchType.LAZY)
+	private SocialProvider socialProvider;
 }
