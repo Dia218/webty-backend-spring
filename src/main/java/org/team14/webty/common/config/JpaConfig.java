1edbb8ce2d6a49064d6b4d2631acf0c337438bb0
segye <tprpek@gmail.com>
2025-02-19 12:16:17 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/org/team14/webty/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package org.team14.webty.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
